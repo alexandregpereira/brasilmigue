e7a793459f9d0153c9ff58f9012f3c62c7bb7266
Alexandre <alexandre.gomes2606@gmail.com>
2018-11-08 03:46:44 -0200

    Plot some data

--- brasil_migue.py
+++ brasil_migue.py
@@ -7,3 +7,13 @@ def invert_no_show_values(df):
     except AttributeError:
         pass
     return df
+
+def build_column_scatter_plot(plt, df_showed_up, df_not_showed_up, column):
+    showed_up_column_counts = df_showed_up[column].value_counts()
+    not_showed_up_column_counts = df_not_showed_up[column].value_counts()
+    plt.scatter(showed_up_column_counts.keys(), showed_up_column_counts.values, alpha=0.5)
+    plt.scatter(not_showed_up_column_counts.keys(), not_showed_up_column_counts.values, alpha=0.5)
+    plt.ylabel('Appointment Quantity')
+    plt.xlabel(column)
+    plt.legend(['Showed up', 'Not showed up'])
+    return plt
